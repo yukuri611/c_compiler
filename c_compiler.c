bea3a2d81ce0f739e3c36ac0ffe0531d88d1ce33
yukuri611 <yukiiw611@gmail.com>
2026-01-20 16:26:43 +0900

    update error message

--- c_compiler.c
+++ c_compiler.c
@@ -85,6 +85,21 @@ Token* tokenize(char* p) {
     return head.next;
 }
 
+char* user_input;
+
+void error_at(char* loc, char* fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "Argument count is incorrect\n");
